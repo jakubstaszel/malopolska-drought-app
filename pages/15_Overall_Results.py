86a6018efb43cca86bfcc303c3d05a9a3ed9d2a0
jakubstaszel <staszel.jakub.98@gmail.com>
2023-06-29 00:52:37 +0200

    Update overall results page

--- pages/15_Overall_Results.py
+++ pages/15_Overall_Results.py
@@ -4,12 +4,17 @@ from imagery.stats import get_means_normalized
 
 
 st.cache_data(ttl=3600)
+
+
 def get_means_normalized_cache():
     return get_means_normalized()
 
+
 st.title("Overall Results")
 
-st.write("Before creating the charts, each index was normalized separately, resulting in values ranging from 0 to 1. Additionally, except for MSI, NMDI, and NDWI v2, there was a sign change, so in every case a higher value indicates a more intense drought.")
+st.write(
+    "Before creating the charts, each index was normalized separately, resulting in values ranging from 0 to 1. Additionally, except for MSI, NMDI, and NDWI v2, there was a sign change, so in every case a higher value indicates a more intense drought."
+)
 
 first_key = get_means_normalized_cache().keys()[0]
 print(first_key)
@@ -17,9 +22,11 @@ print(first_key)
 # selected = st.multiselect("Indexes to be displayed", options, options)
 # st.line_chart(get_means_normalized_cache()[selected])
 
-st.write("Oparte na NIR i SWIR")
-st.line_chart(get_means_normalized_cache()[['ndwi1', 'nmdi', 'ndmi', 'msi']])
-st.write("Oparte na NIR i R")
-st.line_chart(get_means_normalized_cache()[['ndvi', 'wdrvi', 'msavi2']])
-st.write("Inne")
-st.line_chart(get_means_normalized_cache()[['ndwi2', 'evi']])
+st.subheader("All indexes")
+st.line_chart(get_means_normalized_cache())
+st.subheader("Indexes based on NIR and SWIR")
+st.line_chart(get_means_normalized_cache()[["ndwi1", "nmdi", "ndmi", "msi"]])
+st.subheader("Indexes based on NIR and Red")
+st.line_chart(get_means_normalized_cache()[["ndvi", "wdrvi", "msavi2"]])
+st.subheader("Other indexes")
+st.line_chart(get_means_normalized_cache()[["ndwi2", "evi"]])
